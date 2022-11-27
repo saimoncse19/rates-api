ce22f4a94d732d70e987ff9e5b57e0730f292d74
saimoncse19 <saimoncse19@gmail.com>
2022-11-27 18:35:22 +0600

    Add FastAPI startup and shutdown event handlers

--- services/rates-api/src/main.py
+++ services/rates-api/src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from .db import database
+
 app = FastAPI()
 
 
@@ -7,3 +9,15 @@ app = FastAPI()
 async def home():
     """The entrypoint of our Rates API"""
     return {"message": "Welcome! from Rates API."}
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
