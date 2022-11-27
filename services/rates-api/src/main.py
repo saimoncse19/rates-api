088227f8c1b80267b2486c973a7a86739a8ff2c6
saimoncse19 <saimoncse19@gmail.com>
2022-11-27 21:51:25 +0600

    Implement average_rates REST API
    The API calculates average prices between origin and destination for a given day

--- services/rates-api/src/main.py
+++ services/rates-api/src/main.py
@@ -1,6 +1,9 @@
+import datetime
 import logging
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 from .db import database
 
@@ -10,10 +13,99 @@ LOGGER = logging.getLogger(__name__)
 app = FastAPI()
 
 
+class MissingDateException(Exception):
+    """Raise when an expected date is missing"""
+
+
+class MissingCodeOrSlugException(Exception):
+    """Raise when either origin or destination code/slug is missing"""
+
+
+class InvalidDateRangeException(Exception):
+    """Raise when start date is greater than end date."""
+
+
+async def ports_slug_to_code(slug: str) -> list[str]:
+    """
+    Retrieve all the port codes for a given slug that represents a geographic region
+
+    :param slug: A region slug ex: north_europe_main
+    :type slug: str
+    :return: A list of port codes for a given region slug
+    :rtype: list[str]
+    """
+    ports = await database.fetch_all(f"SELECT code FROM ports WHERE parent_slug='{slug}'")
+    return [port.code for port in ports]
+
+
+def get_query_compatible_list(data: list) -> str:
+    """
+    Produce a string representation of a Postgres list to use in query from a Python list
+
+    :param data: a list of data
+    :type data: list
+    :return: a query compatible string representing a postgres list
+    :rtype: str
+    """
+    sub_query = "', '".join(data)
+    return sub_query
+
+
 @app.get("/")
 async def home():
     """The entrypoint of our Rates API"""
     return {"message": "Welcome! from Rates API."}
+
+
+@app.get("/rates")
+async def average_rates(date_from: datetime.date,
+                        date_to: datetime.date,
+                        origin: str,
+                        destination: str) -> JSONResponse:
+    """
+    Retrieve average prices for each day on a route between origin and destination for the given date range
+
+    :param date_from: the start date
+    :type date_from: datetime.date
+    :param date_to: the end date
+    :type date_to: datetime.date
+    :param origin: port code or region slug
+    :type origin: str
+    :param destination: port code or region slug
+    :type destination: str
+    :return: average prices between origin to destination for each day
+    :rtype: JSONResponse
+    :raise MissingDateException: when an expected date is missing
+    :raise InvalidDateRangeException: when date_to predates date_from
+    :raise MissingCodeOrSlugException: when port code or slug is missing
+    """
+    if not date_to or not date_from:
+        LOGGER.error("Either the start or end date is missing.")
+        raise MissingDateException
+    if date_from > date_to:
+        LOGGER.error("Invalid date range, start date is greater than end date.")
+        raise InvalidDateRangeException
+    if not origin:
+        LOGGER.error("Origin code or slug is missing.")
+        raise MissingCodeOrSlugException
+    if not destination:
+        LOGGER.error("Destination code or slug is missing.")
+        raise MissingCodeOrSlugException
+
+    origins = [origin] if origin == origin.upper() else \
+        await ports_slug_to_code(origin)
+    destinations = [destination] if destination == destination.upper() else \
+        await ports_slug_to_code(destination)
+
+    query = f"""SELECT day, (CASE WHEN COUNT(*) > 2 THEN avg(price) ELSE null END) as average_price 
+    FROM prices WHERE orig_code IN ('{get_query_compatible_list(origins)}')
+    and dest_code IN ('{get_query_compatible_list(destinations)}')
+    and day BETWEEN '{date_from}' and  '{date_to}' GROUP BY day
+    """
+
+    average_prices_objs = await database.fetch_all(query)
+    average_prices = jsonable_encoder(average_prices_objs)
+    return JSONResponse(content=average_prices)
 
 
 @app.on_event("startup")
