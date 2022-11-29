11c1b3e5f665169092112fce945846e97dfae495
saimoncse19 <saimoncse19@gmail.com>
2022-11-29 20:35:06 +0600

    Update test cases for the routes

--- services/rates-api/tests/conftest.py
+++ services/rates-api/tests/conftest.py
@@ -14,49 +14,7 @@ engine = sqlalchemy.create_engine(DATABASE_URL)
 metadata.create_all(engine)
 
 
-@pytest.fixture
-def mock_rate_response():
-
-    mock_response = [
-        {
-          "day": "2016-01-01",
-          "average_price": 1310.2200886262924
-        },
-        {
-          "day": "2016-01-02",
-          "average_price": 1310.1875923190546
-        },
-        {
-          "day": "2016-01-05",
-          "average_price": 1312.2635658914728
-        },
-        {
-          "day": "2016-01-06",
-          "average_price": 1309.2899224806201
-        },
-        {
-          "day": "2016-01-07",
-          "average_price": 1312.2635658914728
-        },
-        {
-          "day": "2016-01-08",
-          "average_price": 1311.843410852713
-        },
-        {
-          "day": "2016-01-09",
-          "average_price": 1330.838124054463
-        },
-        {
-          "day": "2016-01-10",
-          "average_price": 1332.2537764350452
-        }
-    ]
-
-    return mock_response
-
-
 @pytest.fixture(scope="module")
 def mock_app():
     with TestClient(app) as client:
         yield client
-
--- services/rates-api/tests/test_main.py
+++ services/rates-api/tests/test_main.py
@@ -1,11 +1,11 @@
-class TestHome:
+class TestHomeRoute:
     def test_home(self, mock_app):
         response = mock_app.get("/")
         assert response.status_code == 200
         assert response.json() == {"message": "Welcome! from Rates API."}
 
 
-class TestAverageRates:
+class TestRatesRoute:
     def test_with_missing_query_parameters(self, mock_app):
         response = mock_app.get("/rates?date_from=2021-01-01&date_to=2021-01-10&origin=china_main")
         assert response.status_code == 422
@@ -24,3 +24,9 @@ class TestAverageRates:
                                 "destination=northern_europe")
         assert response.status_code == 400
         assert response.json() == {"detail": "Invalid date range, start date is greater than end date."}
+
+    def test_with_valid_query_parameters_succeed(self, mock_app):
+        response = mock_app.get("/rates?date_from=2021-01-01&date_to=2021-01-10&origin=china_main&"
+                                "destination=northern_europe")
+        assert response.status_code == 200
+
